deb81246fc8b8a8c5d8a9662834d575bc053c101
wessware <stevenshisia@yahoo.com>
2024-03-29 06:23:03 +0300

    project set up

---
+++ app.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+
+const express = require("express")
+const expressLayouts = require('express-ejs-layouts')
+
+const app= express()
+const port = 5000 || process.env.PORT 
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+//static files
+app.use(express.static('public'))
+
+//templating engine
+app.use(expressLayouts)
+app.set("layout", './layouts/main')
+app.set('view engine', 'ejs')
+
+app.get('/', function(req, res){
+    res.render('index');
+})
+
