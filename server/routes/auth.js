f622625cf031709efa3b535e315168a606f6348c
wessware <stevenshisia@yahoo.com>
2024-04-04 18:33:03 +0300

    finding and creating a user

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -9,23 +9,43 @@ passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: process.env.GOOGLE_CALLBACK_URL
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    
-    console.log(profile);
-  }
+},
+    async function (accessToken, refreshToken, profile, done) {
+
+        const newUser = {
+            googleId: profile.id,
+            displayName: profile.displayName,
+            firstName: profile.name.givenName,
+            lastName: profile.name.familyName,
+            profileImage: profile.photos[0].value
+        }
+        try {
+            let user = await User.findOne({ googleId: profile.id })
+            if (user) {
+                done(null, user);
+            } else {
+                user = await User.create(newUser);
+                done(null, user);
+            }
+
+        } catch (error) {
+            console.log(error)
+        }
+
+        console.log(profile);
+    }
 ));
 
 // Google Login Route
 router.get('/auth/google',
-  passport.authenticate('google', { scope: ['email', 'profile'] }));
+    passport.authenticate('google', { scope: ['email', 'profile'] }));
 
-  //Retrieve user data from google
-router.get('/google/callback', 
-  passport.authenticate('google', { 
-    failureRedirect: '/login-failure',
-    successRedirect: '/dashboard'
-})
+//Retrieve user data from google
+router.get('/google/callback',
+    passport.authenticate('google', {
+        failureRedirect: '/login-failure',
+        successRedirect: '/dashboard'
+    })
 );
 
 //route if something goes wrong
@@ -34,16 +54,16 @@ router.get('/login-failure', (req, res) => {
 });
 
 //presist user data after successfull authentication
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
     done(null, user);
-  });
+});
 
-  //retrieve user data from session
-  passport.deserializeUser(function(user, done) {
-    user.FindById(id, function(err, user) {
-      done(err, user);
+//retrieve user data from session
+passport.deserializeUser(function (user, done) {
+    user.FindById(id, function (err, user) {
+        done(err, user);
     });
-  });
+});
 
 
 module.exports = router;
