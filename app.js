bfd9b4339d2bdc25e3fde825bcc82b787366f289
wessware <stevenshisia@yahoo.com>
2024-04-04 17:59:41 +0300

    authentication script

--- app.js
+++ app.js
@@ -1,38 +1,45 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require("express")
-const expressLayouts = require('express-ejs-layouts')
+const express = require("express");
+const expressLayouts = require('express-ejs-layouts');
 
-const connectDB = require('./server/config/db')
+const connectDB = require('./server/config/db');
+const session = require('express-session');
+const passport = require("passport");
+const MongoStore = require('connect-mongo');
 
-const app= express()
-const port = 5000 || process.env.PORT 
+const app= express();
+const port = 5000 || process.env.PORT;
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //connect to the database
-connectDB()
+connectDB();
 
 //static files
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 //templating engine
-app.use(expressLayouts)
-app.set("layout", './layouts/main')
-app.set('view engine', 'ejs')
+app.use(expressLayouts);
+app.set("layout", './layouts/main');
+app.set('view engine', 'ejs');
 
 //routes
-app.use('/', require('./server/routes/index'))
-app.use('/', require('./server/routes/dashboard'))
+app.use('/', require('./server/routes/index'));
+app.use('/', require('./server/routes/auth'));
+app.use('/', require('./server/routes/dashboard'));
 
 
 //Handle 404
 app.get('*', function(req, res) {
     //res.status(404).send("404 Page Not Found!")
     res.status(404).render("404")
-})
+});
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
-})
+});
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,10 @@
-const express = require("express")
-const router = express.Router()
-const mainController = require( "../controllers/mainController")
+const express = require("express");
+const router = express.Router();
+const mainController = require( "../controllers/mainController");
 
 //App Routes
-router.get('/', mainController.homepage) //Home Page
-router.get('/about', mainController.about) //about Page
+router.get('/', mainController.homepage); //Home Page
+router.get('/about', mainController.about); //about Page
 
-module.exports = router
+module.exports = router;
 
