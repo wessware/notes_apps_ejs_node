08d1e755f9baa0d0c6ea0481a8961afc24dd8812
wessware <stevenshisia@yahoo.com>
2024-04-04 18:25:34 +0300

    user model 1

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const passport = require("passport");
 
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require("../models/Users");
 
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
